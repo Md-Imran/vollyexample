079e6fbb60151ce39e66f8d44f043b6ba3457021
Imran <imranku11@gmail.com>
2018-04-26 17:29:14 +0600

    init commit

---
+++ app/src/main/java/com/example/imran/vollyexample/MainActivity.java
@@ -0,0 +1,382 @@
+package com.example.imran.vollyexample;
+
+import android.app.ProgressDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.NetworkError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.example.imran.vollyexample.app.AppController;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.UnsupportedEncodingException;
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+
+    // json object response url
+    private String urlJsonObj = "https://api.androidhive.info/volley/person_object.json";
+    private String BASE_URL = "https://reqres.in";
+    // json array response url
+    private String urlJsonArry = "https://api.androidhive.info/volley/person_array.json";
+
+    private static String TAG = MainActivity.class.getSimpleName();
+    private Button btnMakeObjectRequest, btnMakeArrayRequest, btnPostRequest;
+
+
+    // Progress dialog
+    private ProgressDialog pDialog;
+
+    private TextView txtResponse;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        bindView();
+        buttonAction();
+    }
+
+    private void bindView() {
+        btnMakeObjectRequest = (Button) findViewById(R.id.btnObjRequest);
+        btnMakeArrayRequest = (Button) findViewById(R.id.btnArrayRequest);
+        btnPostRequest = (Button) findViewById(R.id.btnPostRequest);
+        txtResponse = (TextView) findViewById(R.id.txtResponse);
+
+        pDialog = new ProgressDialog(this);
+        pDialog.setMessage("Please wait...");
+        pDialog.setCancelable(false);
+    }
+
+    private void buttonAction() {
+        btnMakeObjectRequest.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // making json object request
+                makeJsonObjectRequest();
+            }
+        });
+
+        btnMakeArrayRequest.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // making json array request
+                makeJsonArrayRequest();
+            }
+        });
+
+        btnPostRequest.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // making post request
+                //postRequest();
+                try {
+                    registration();
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    /**
+     * Method to make json object request where json response starts wtih {
+     */
+    private void makeJsonObjectRequest() {
+        showpDialog();
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, urlJsonObj, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Log.d(TAG, response.toString());
+                        try {
+                            String name = response.getString("name");
+                            String email = response.getString("email");
+                            JSONObject phone = response.getJSONObject("phone");
+                            String home = phone.getString("home");
+                            String mobile = phone.getString("mobile");
+
+                            StringBuilder stringBuilder = new StringBuilder();
+                            stringBuilder.append("Name : " + name + " Email " + email +
+                                    " Home " + home + " Mobile " + mobile);
+                            txtResponse.setText(stringBuilder);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                            Toast.makeText(getApplicationContext(), "Got Jeson Exception" + e.toString(), Toast.LENGTH_SHORT).show();
+                        }
+                        hidepDialog();
+
+
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                VolleyLog.d(TAG, "Error: " + error.getMessage());
+                Toast.makeText(getApplicationContext(),
+                        error.getMessage(), Toast.LENGTH_SHORT).show();
+                // hide the progress dialog
+                hidepDialog();
+            }
+        });
+        // Adding request to request queue
+        AppController.getInstance().addToRequestQueue(jsonObjectRequest);
+    }
+
+    /**
+     * Method to make json array request where response starts with [
+     */
+    private void makeJsonArrayRequest() {
+
+        showpDialog();
+        final StringBuilder stringBuilder = new StringBuilder();
+
+
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(urlJsonArry, new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response) {
+
+                for (int i = 0; i < response.length(); i++) {
+                    try {
+                        JSONObject jsonObject = (JSONObject) response.get(i);
+                        String name = jsonObject.getString("name");
+                        String email = jsonObject.getString("email");
+                        JSONObject phone = jsonObject.getJSONObject("phone");
+                        String home = phone.getString("home");
+                        String mobile = phone.getString("mobile");
+                        stringBuilder.append("Name : " + name + " Email " + email +
+                                " Home " + home + " Mobile " + mobile);
+
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                    hidepDialog();
+                }
+
+                txtResponse.setText(stringBuilder);
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                VolleyLog.d(TAG, "Error: " + error.getMessage());
+                Toast.makeText(getApplicationContext(),
+                        error.getMessage(), Toast.LENGTH_SHORT).show();
+                hidepDialog();
+            }
+        });
+
+        AppController.getInstance().addToRequestQueue(jsonArrayRequest);
+    }
+
+    private void POSTStringAndJSONRequest() {
+
+        ///  RequestQueue queue = SingletonRequestQueue.getInstance(getApplicationContext()).getRequestQueue();
+
+        VolleyLog.DEBUG = true;
+        String uri = BASE_URL + "/api/users";
+
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, uri, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                VolleyLog.wtf(response, "utf-8");
+                Toast.makeText(getApplicationContext(), response, Toast.LENGTH_LONG).show();
+
+            }
+
+        }, errorListener) {
+
+            public Priority getPriority() {
+                return Priority.LOW;
+            }
+
+            @Override
+            public Map getParams() {
+                Map params = new HashMap();
+
+                params.put("name", "Anupam");
+                params.put("job", "Android Developer");
+
+
+                return params;
+            }
+
+            @Override
+            public Map getHeaders() throws AuthFailureError {
+                HashMap headers = new HashMap();
+                headers.put("Content-Type", "application/x-www-form-urlencoded; charset=utf-8");
+                return headers;
+            }
+
+        };
+
+
+        JSONObject jsonObject = new JSONObject();
+        try {
+            jsonObject.put("name", "JournalDev.com");
+            jsonObject.put("job", "To teach you the best");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(uri, jsonObject, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                VolleyLog.wtf(response.toString(), "utf-8");
+                Toast.makeText(getApplicationContext(), response.toString(), Toast.LENGTH_LONG).show();
+
+            }
+        }, errorListener) {
+            public int getMethod() {
+                return Method.POST;
+            }
+
+            @Override
+            public Priority getPriority() {
+                return Priority.NORMAL;
+            }
+        };
+
+
+        StringRequest stringRequestPOSTJSON = new StringRequest(Request.Method.POST, uri, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                VolleyLog.wtf(response);
+                Toast.makeText(getApplicationContext(), response, Toast.LENGTH_LONG).show();
+
+            }
+        }, errorListener) {
+            public Request.Priority getPriority() {
+                return Request.Priority.HIGH;
+            }
+
+            @Override
+            public Map getHeaders() throws AuthFailureError {
+                HashMap headers = new HashMap();
+                headers.put("Content-Type", "application/json; charset=utf-8");
+                return headers;
+            }
+
+            @Override
+            public byte[] getBody() throws AuthFailureError {
+
+                JSONObject jsonObject = new JSONObject();
+                try {
+                    jsonObject.put("name", "Android Tutorials");
+                    jsonObject.put("job", "To implement Volley in an Android Application.");
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+                String requestBody = jsonObject.toString();
+
+
+                try {
+                    return requestBody == null ? null : requestBody.getBytes("utf-8");
+                } catch (UnsupportedEncodingException uee) {
+                    VolleyLog.wtf("Unsupported Encoding while trying to get the bytes of %s using %s", requestBody, "utf-8");
+                    return null;
+                }
+            }
+
+
+        };
+
+        AppController.getInstance().addToRequestQueue(stringRequest);
+        AppController.getInstance().addToRequestQueue(jsonObjectRequest);
+        AppController.getInstance().addToRequestQueue(stringRequestPOSTJSON);
+        //   queue.add(stringRequest);
+        //   queue.add(jsonObjectRequest);
+        // queue.add(stringRequestPOSTJSON);
+    }
+
+    private void showpDialog() {
+        if (!pDialog.isShowing())
+            pDialog.show();
+    }
+
+    private void hidepDialog() {
+        if (pDialog.isShowing())
+            pDialog.dismiss();
+    }
+
+    Response.ErrorListener errorListener = new Response.ErrorListener() {
+        @Override
+        public void onErrorResponse(VolleyError error) {
+            if (error instanceof NetworkError) {
+                Toast.makeText(getApplicationContext(), "No network available", Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(getApplicationContext(), error.toString(), Toast.LENGTH_LONG).show();
+            }
+        }
+    };
+
+    private void postRequest() {
+
+        String url = BASE_URL + "/api/users";
+        StringRequest postRequest = new StringRequest(Request.Method.POST, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // response
+                        Log.d("Response", response);
+                    }
+                },
+                errorListener
+        ) {
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("name", "Imran");
+                params.put("domain", "http://itsalif.info");
+                params.put("address", "221/Baker street");
+
+                return params;
+            }
+
+
+        };
+        AppController.getInstance().addToRequestQueue(postRequest);
+
+    }
+
+    private void registration() throws JSONException {
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("email", "sydney@fife");
+        jsonObject.put("password", "pistol");
+        String uri = BASE_URL + "/api/register";
+        JsonObjectRequest jsonObjectPostRequest = new JsonObjectRequest(Request.Method.POST, uri, jsonObject, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                Toast.makeText(getApplicationContext(), response.toString(), Toast.LENGTH_LONG).show();
+            }
+        }, errorListener) {
+            @Override
+            public Request.Priority getPriority() {
+                return Request.Priority.NORMAL;
+            }
+        };
+        AppController.getInstance().addToRequestQueue(jsonObjectPostRequest);
+
+    }
+}
---
+++ app/src/main/java/com/example/imran/vollyexample/app/AppController.java
@@ -0,0 +1,51 @@
+package com.example.imran.vollyexample.app;
+
+import android.app.Application;
+import android.text.TextUtils;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+
+/**
+ * Created by Imran on 4/26/2018.
+ */
+
+public class AppController extends Application {
+
+    private RequestQueue mRequestQueue;
+    private static AppController appcontrolinstance;
+    public static String TAG = AppController.class.getCanonicalName();
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        appcontrolinstance = this;
+    }
+
+    public static synchronized AppController getInstance() {
+        return appcontrolinstance;
+    }
+
+    public RequestQueue getmRequestQueue() {
+        if (mRequestQueue == null) {
+            mRequestQueue = Volley.newRequestQueue(getApplicationContext());
+        }
+        return mRequestQueue;
+    }
+
+    public <T> void addToRequestQueue(Request<T> request, String tag) {
+        request.setTag(TextUtils.isEmpty(tag) ? TAG : tag);
+        getmRequestQueue().add(request);
+    }
+    public <T> void addToRequestQueue(Request<T> req) {
+        req.setTag(TAG);
+        getmRequestQueue().add(req);
+    }
+
+    public void cancelPendingRequests(Object tag) {
+        if (mRequestQueue != null) {
+            mRequestQueue.cancelAll(tag);
+        }
+    }
+}
